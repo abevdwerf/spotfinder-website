0be5a747d33772639231f305745430cc2987199a
Cas Goorman <casgo98@hotmail.nl>
2021-12-02 12:11:48 +0100

    added database migrations

---
+++ database/migrations/2021_12_02_093839_create_desks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDesksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('desks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('room_id')->constrained();
+            $table->integer('available_spaces');
+            $table->integer('wall_outlet');
+            $table->integer('x');
+            $table->integer('y');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('desks');
+    }
+}
