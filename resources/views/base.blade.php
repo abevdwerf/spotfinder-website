0d0b6e566598a7123e3e1cf583a15cbfdf852f81
Rubenvandeven2001 <484553@student.fontys.nl>
2021-12-03 10:47:54 +0100

    Disable scroll-lock (temporary)

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="" lang="en">
+<html class="html--scale-based" lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
