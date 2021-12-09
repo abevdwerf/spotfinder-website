37e70b5da1948db7a6f5bd5841ad8b134d343894
Rubenvandeven2001 <484553@student.fontys.nl>
2021-12-09 15:50:36 +0100

    Styling bumps

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
     <section class="finder">
+        <a href="#" class="finder__close">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z" />
+            </svg>
+        </a>
+
         <a href="#" class="finder__collapse">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                 <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
@@ -126,60 +132,58 @@
     </section>
 
     <main>
-        <div style="position: relative;">
-            <div class="dashboard__first-view">
-                @if(Auth::Check())
-                    <h2 class="h1">Welcome @php echo Auth::User()->name @endphp</h2>
-                @endif
-    
-                <h3 class="h3">Search history</h3>
-                <div class="history flex">
-                    <a href="#" class="box history__item">
-                        <span class="history__location">Fontys R10, Rachelsmolen</span>
-                        <h4 class="h4">Silent room - 2 people</h4>
-                    </a>
-                </div>
-    
-                <h3 class="h3" style="margin-top: 3rem;">Your reservations</h3>
-                <div class="reservations flex">
-                    @for ($reservations = 0; $reservations < 3; $reservations++)
-                        <a href="#" class="reservation__item box">
-                            <span class="reservation__location">Fontys R10, Rachelsmolen</span>
-                            <div class="flex" style="justify-content: flex-start;">
-                                <h4 class="h4">OIL 0.12</h4>
-    
-                                <div class="flex">
-                                    <div class="reservation__type">
-                                        <div class="flex">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="14.183" height="17" viewBox="0 0 14.183 17">
-                                                <path d="M91.126,8.189a3.962,3.962,0,0,0,2.9-1.2,3.962,3.962,0,0,0,1.2-2.9,3.962,3.962,0,0,0-1.2-2.9,4.094,4.094,0,0,0-5.79,0,3.962,3.962,0,0,0-1.2,2.895,3.962,3.962,0,0,0,1.2,2.9A3.963,3.963,0,0,0,91.126,8.189Zm0,0" transform="translate(-84.142)" />
-                                                <path d="M14.149,252.177a10.114,10.114,0,0,0-.138-1.074,8.464,8.464,0,0,0-.264-1.08,5.335,5.335,0,0,0-.444-1.007,3.8,3.8,0,0,0-.67-.872,2.952,2.952,0,0,0-.962-.6,3.324,3.324,0,0,0-1.228-.222,1.246,1.246,0,0,0-.665.282c-.2.13-.433.281-.693.447a3.971,3.971,0,0,1-.9.4,3.483,3.483,0,0,1-2.194,0,3.962,3.962,0,0,1-.9-.4c-.258-.165-.491-.315-.694-.447a1.245,1.245,0,0,0-.665-.282,3.319,3.319,0,0,0-1.228.222,2.949,2.949,0,0,0-.962.6,3.8,3.8,0,0,0-.669.872,5.345,5.345,0,0,0-.444,1.007,8.484,8.484,0,0,0-.264,1.08,10.078,10.078,0,0,0-.138,1.075c-.023.325-.034.663-.034,1a2.823,2.823,0,0,0,.838,2.136A3.018,3.018,0,0,0,3,256.1h8.186a3.017,3.017,0,0,0,2.16-.788,2.821,2.821,0,0,0,.839-2.136c0-.343-.012-.68-.034-1Zm0,0" transform="translate(0 -239.104)" />
-                                            </svg>
-    
-                                            <span class="reservation__type-text">meeting room</span>
-                                        </div>
+        <div class="dashboard__first-view">
+            @if(Auth::Check())
+                <h2 class="h1">Welcome @php echo Auth::User()->name @endphp</h2>
+            @endif
+
+            <h3 class="h3">Search history</h3>
+            <div class="history flex">
+                <a href="#" class="box history__item">
+                    <span class="history__location">Fontys R10, Rachelsmolen</span>
+                    <h4 class="h4">Silent room - 2 people</h4>
+                </a>
+            </div>
+
+            <h3 class="h3" style="margin-top: 3rem;">Your reservations</h3>
+            <div class="reservations flex">
+                @for ($reservations = 0; $reservations < 3; $reservations++)
+                    <a href="#" class="reservation__item box">
+                        <span class="reservation__location">Fontys R10, Rachelsmolen</span>
+                        <div class="flex" style="justify-content: flex-start;">
+                            <h4 class="h4">OIL 0.12</h4>
+
+                            <div class="flex">
+                                <div class="reservation__type">
+                                    <div class="flex">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14.183" height="17" viewBox="0 0 14.183 17">
+                                            <path d="M91.126,8.189a3.962,3.962,0,0,0,2.9-1.2,3.962,3.962,0,0,0,1.2-2.9,3.962,3.962,0,0,0-1.2-2.9,4.094,4.094,0,0,0-5.79,0,3.962,3.962,0,0,0-1.2,2.895,3.962,3.962,0,0,0,1.2,2.9A3.963,3.963,0,0,0,91.126,8.189Zm0,0" transform="translate(-84.142)" />
+                                            <path d="M14.149,252.177a10.114,10.114,0,0,0-.138-1.074,8.464,8.464,0,0,0-.264-1.08,5.335,5.335,0,0,0-.444-1.007,3.8,3.8,0,0,0-.67-.872,2.952,2.952,0,0,0-.962-.6,3.324,3.324,0,0,0-1.228-.222,1.246,1.246,0,0,0-.665.282c-.2.13-.433.281-.693.447a3.971,3.971,0,0,1-.9.4,3.483,3.483,0,0,1-2.194,0,3.962,3.962,0,0,1-.9-.4c-.258-.165-.491-.315-.694-.447a1.245,1.245,0,0,0-.665-.282,3.319,3.319,0,0,0-1.228.222,2.949,2.949,0,0,0-.962.6,3.8,3.8,0,0,0-.669.872,5.345,5.345,0,0,0-.444,1.007,8.484,8.484,0,0,0-.264,1.08,10.078,10.078,0,0,0-.138,1.075c-.023.325-.034.663-.034,1a2.823,2.823,0,0,0,.838,2.136A3.018,3.018,0,0,0,3,256.1h8.186a3.017,3.017,0,0,0,2.16-.788,2.821,2.821,0,0,0,.839-2.136c0-.343-.012-.68-.034-1Zm0,0" transform="translate(0 -239.104)" />
+                                        </svg>
+
+                                        <span class="reservation__type-text">meeting room</span>
                                     </div>
                                 </div>
                             </div>
-                            <span class="reservation__time flex" style="justify-content: flex-start;">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-                                    <path d="M11.375,3.375a8,8,0,1,0,8,8A8,8,0,0,0,11.375,3.375Zm.538,8.692a.54.54,0,0,1-.538.538H7.683a.538.538,0,0,1,0-1.077h3.154V6.452a.538.538,0,0,1,1.077,0Z" transform="translate(-3.375 -3.375)" />
-                                </svg>
-    
-                                <span>13:00 - 14:00</span>
-                            </span>
-                        </a>
-                    @endfor
-                </div>
-            </div>
-    
-            <div class="dashboard__search-result">
-                <h3 class="h3">Available workspaces</h3>
+                        </div>
+                        <span class="reservation__time flex" style="justify-content: flex-start;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+                                <path d="M11.375,3.375a8,8,0,1,0,8,8A8,8,0,0,0,11.375,3.375Zm.538,8.692a.54.54,0,0,1-.538.538H7.683a.538.538,0,0,1,0-1.077h3.154V6.452a.538.538,0,0,1,1.077,0Z" transform="translate(-3.375 -3.375)" />
+                            </svg>
 
-                <div class="dashboard__content-wrap flex">
-                    <div class="dashboard__search-inner"></div>
-                    <div class="dashboard__search-detailed"></div>
-                </div>
+                            <span>13:00 - 14:00</span>
+                        </span>
+                    </a>
+                @endfor
+            </div>
+        </div>
+
+        <div class="dashboard__search-result">
+            <h3 class="h3">Available workspaces</h3>
+
+            <div class="dashboard__content-wrap flex">
+                <div class="dashboard__search-inner"></div>
+                <div class="dashboard__search-detailed"></div>
             </div>
         </div>
     </main>
